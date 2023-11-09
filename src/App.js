f8aef3d1d61290916d192dcf43ff8603a1a407b9
asma zabeen <99760535+asmazabeen13@users.noreply.github.com>
2023-11-09 20:25:22 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Navbar from "./Components/Navbar";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
